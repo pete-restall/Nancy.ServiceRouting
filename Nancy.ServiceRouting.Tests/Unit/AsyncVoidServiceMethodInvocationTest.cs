e0eb4bb4e7038c5dcefd3f8b4b533d34aa657ee1
pete-restall <pete@restall.net>
2014-05-11 14:41:55 +0100

    Bumped up the async test safety timeouts to 10 seconds as the AppVeyor CI server seems to be a little on the slow side...

--- Nancy.ServiceRouting.Tests/Unit/AsyncVoidServiceMethodInvocationTest.cs
+++ Nancy.ServiceRouting.Tests/Unit/AsyncVoidServiceMethodInvocationTest.cs
@@ -64,7 +64,7 @@ namespace Restall.Nancy.ServiceRouting.Tests.Unit
 
 		public class Response { }
 
-		private const int AsyncSafetyTimeoutSeconds = 5;
+		private const int AsyncSafetyTimeoutSeconds = 10;
 
 		private const int AsyncSafetyTimeoutMilliseconds = AsyncSafetyTimeoutSeconds * 1000;
 
